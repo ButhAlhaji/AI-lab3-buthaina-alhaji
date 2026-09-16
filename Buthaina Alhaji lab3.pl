b5920e54a4bf1bba7f7fac6c63bd3ac5b1067f31
ButhAlhaji <alhaji.b.h.m@gmail.com>
2026-09-16 15:11:26 +0300

    Add files via upload

---
+++ Buthaina Alhaji lab3.pl
@@ -0,0 +1,40 @@
+female(jory).
+female(reem).
+female(noof).
+female(rana).
+male(khaled).
+male(ahmed).
+male(mohammed).
+male(saad).
+
+parent(ahmad,saad).
+parent(ahmad,rana).
+parent(noof,saad).
+parent(noof,rana).
+parent(jory,reem).
+parent(jory,noof).
+parent(jory,mohammed).
+parent(khaled,reem).
+parent(khaled,noof).
+parent(khaled,mohammed).
+
+father(X, Y) :-
+    parent(X, Y),
+    male(X).
+
+mother(X, Y) :-
+    parent(X, Y),
+    female(X).
+
+brother(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    male(X),
+    X \= Y.
+
+sister(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    female(X),
+    X \= Y.
+
